be8c98c2666cf07606814e4a0f7132c206218ceb
Piotr Krogulec <krogulec.piotr@gmail.com>
2015-05-21 13:09:16 +0200

    Refaktoryzacja MainPage i zmiana obsługi przycisków dodawania i zatwierdzania listy

--- eBuyListApplication/MainPage.xaml.cs
+++ eBuyListApplication/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using eBuyListApplication.Model;
 using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Shell;
 
 namespace eBuyListApplication
 {
@@ -14,99 +15,63 @@ namespace eBuyListApplication
     {
         public static EBuyListsManager Manager = new EBuyListsManager();
 
-        // Constructor
         public MainPage()
         {
             InitializeComponent();          
             MainLongListSelector.DataContext = Manager.GetAllLists();           
         }
 
-        // Load data for the ViewModel Items
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            //if (MainLongListSelector.DataContext == null)
-            //{
-            //    MainLongListSelector.DataContext = _manager.GetAllLists();
-            //}
 
             MainLongListSelector.DataContext = null;
             MainLongListSelector.DataContext = Manager.GetAllLists();
 
-            //TODO Załadowanie danych jak przechodzi się na tę stronę z innej
         }
 
-        // Handle selection changed on LongListSelector
         private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // If selected item is null (no selection) do nothing
             if (MainLongListSelector.SelectedItem == null)
                 return;
 
-            // Navigate to the new page
             NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as EBuyList).Id, UriKind.Relative));
 
-
-            // Reset selected item to null (no selection)
             MainLongListSelector.SelectedItem = null;
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
-
         private void AddBarIconButton_OnClick(object sender, EventArgs e)
         {
-            AddNewListTextBox.Height = 100;
-            AddNewListTextBox.BorderBrush = new SolidColorBrush(Colors.Black);
-            AddNewListTextBox.FontSize = 20;
-            AddNewListTextBox.Visibility = Visibility.Visible;
-
-
-            AddNewListButton.Height = 70;
-            AddNewListButton.Content = "Zapisz listę";
-            AddNewListButton.Background = new SolidColorBrush(Colors.DarkGray);
-            AddNewListButton.Visibility = Visibility.Visible;
-
-            AddNewListButton.Click += addNewListButton_Click;      
-        }
-
-        void addNewListButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (Manager.GetAllLists().Count > 5)
+            if (Manager.GetAllLists().Count >= 5)
             {
-                MessageBox.Show("Możesz maksymalnie dodać 5 list"); 
+                MessageBox.Show("Możesz dodać maksymalnie 5 list");
+                return;
             }
 
             else
             {
+                AddTextBoxForAddingNewList();
+                AddBarButton().IsEnabled = false;
+            }
+
+        }
+
+        void ConfirmAddingListAppBarButton_Click(object sender, EventArgs e)
+        {
+
                 if (AddNewListTextBox.Text != "")
                 {
+                    (sender as ApplicationBarIconButton).IsEnabled = true;
                     var newListIndex = Manager.AddNewList(AddNewListTextBox.Text);
-
                     NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + newListIndex, UriKind.Relative));
-
                     AddNewListTextBox.Text = "";
-
                     AddNewListTextBox.Visibility = Visibility.Collapsed;
-                    AddNewListButton.Visibility = Visibility.Collapsed;
+                    AddBarButton().IsEnabled = true;
                 }
                 else
                 {
                     return;
                 }
-            }
+
         }
 
         private void Edit_OnClick(object sender, RoutedEventArgs e)
@@ -137,6 +102,42 @@ namespace eBuyListApplication
 
             sms.Body = smsListBody;
             sms.Show();
+        }
+
+
+        private void AddNewListTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (AddNewListTextBox.Text != "")
+            {
+                ConfirmBarButton().IsEnabled = true;
+            }
+
+            else 
+            {
+                ConfirmBarButton().IsEnabled = false;
+            }
+        }
+
+
+
+        private void AddTextBoxForAddingNewList()
+        {
+            AddNewListTextBox.Height = 100;
+            AddNewListTextBox.BorderBrush = new SolidColorBrush(Colors.Black);
+            AddNewListTextBox.FontSize = 20;
+            AddNewListTextBox.Visibility = Visibility.Visible;
+        }
+
+        private ApplicationBarIconButton AddBarButton()
+        {
+            ApplicationBarIconButton button = (ApplicationBarIconButton)ApplicationBar.Buttons[0];
+            return button;
+        }
+
+        private ApplicationBarIconButton ConfirmBarButton()
+        {
+            ApplicationBarIconButton button = (ApplicationBarIconButton)ApplicationBar.Buttons[1];
+            return button;
         }
     }
 }
